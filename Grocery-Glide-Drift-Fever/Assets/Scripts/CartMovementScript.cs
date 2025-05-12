f5305bf129b0a2045fa469ca8b2c759353352614
Scar <scarcheekttgm@gmail.com>
2025-05-12 10:56:21 +0200

    Revert "Should remove deltatime redundancy again introduced in #840ca04"
    This reverts commit 6eb5b9999b04f0183ef33c78536614e1f15bec09.

--- Grocery-Glide-Drift-Fever/Assets/Scripts/CartMovementScript.cs
+++ Grocery-Glide-Drift-Fever/Assets/Scripts/CartMovementScript.cs
@@ -25,12 +25,12 @@ public class CartMovement : MonoBehaviour
 	[SerializeField] private float weightMax = 100;
 	[SerializeField] private float weight;
 	[SerializeField] private float minDrift = 5;
-	[SerializeField] private float minBoost;
-	[SerializeField] private float maxBoostStrength;
-	[SerializeField] private float emptyTippingThreshold;
-	[SerializeField] private float fullTippingThreshold;
-	[SerializeField] private float fixedTippingThreshold;
-	[SerializeField] private float maxTippingThresholdBoost;
+ 	[SerializeField] private float minBoost;
+    [SerializeField] private float maxBoostStrength;
+    [SerializeField] private float emptyTippingThreshold;
+    [SerializeField] private float fullTippingThreshold;
+ 	[SerializeField] private float fixedTippingThreshold;
+    [SerializeField] private float maxTippingThresholdBoost;
 	public bool IsDrifting { get; private set; } = false;
 	public bool BoostReady { get; private set; } = false;
 	public bool IsUpright { get; private set; } = false;
@@ -38,10 +38,10 @@ public class CartMovement : MonoBehaviour
 	[Header("DEBUG")]
 	public bool ragdoll = false;
 	[SerializeField] private bool _propUp = false;
-	[SerializeField] private float _tippingThreshold;
-	[SerializeField] private float _driftBoost = 1;
-	[SerializeField] private float _driftValue;
-	[SerializeField] private float _driftScore;
+ 	[SerializeField] private float _tippingThreshold;
+	[SerializeField] private float _driftBoost = 1; 
+	[SerializeField] private float _driftValue; 
+ 	[SerializeField] private float _driftScore;
 	[SerializeField] private float _boostDecaySpeed;
 	[SerializeField] private float _tippingDecaySpeed;
 
@@ -50,7 +50,7 @@ public class CartMovement : MonoBehaviour
 
 	#endregion
 	// Utility vars
-	private Quaternion _lastRot = Quaternion.identity;
+	private Quaternion  _lastRot = Quaternion.identity;
 	private Vector3 _propUpTargetPosition;
 	#endregion
 
@@ -67,26 +67,30 @@ public class CartMovement : MonoBehaviour
 	}
 
 
-	private float _verticalAxis, _horizontalAxis;
+	private float  _verticalAxis, _horizontalAxis;	
 	private void Update()
 	{
+		if(!ragdoll){
+			_verticalAxis = Input.GetAxisRaw("Vertical");
+			_horizontalAxis = Input.GetAxisRaw("Horizontal");
+			
+			_driftValue = DriftValue();
+			switch (_verticalAxis)
+			{
+				//thrust
+				case > 0:
+					_cart.AddForce(transform.forward * (_verticalAxis * thrust * _driftBoost * 110f * Time.deltaTime));
+					break;
+				//brake
+				case < 0:
+					_cart.AddForce(transform.forward * (_verticalAxis * thrust * _driftBoost * 40f * Time.deltaTime));
+					break;
+			}
 
-
-		else if (Input.GetKeyDown("r"))
-		{
-			_propUpTargetPosition = transform.position + new Vector3(0, 1, 0);
-			_cart.isKinematic = true;
-			_propUp = true;
-			// _lastRot.y = 0;
-			_lastRot.x = 0;
-			_lastRot.z = 0;
-		}
-	}
-	private void FixedUpdate()
-	{
-		if (!ragdoll)
-		{
-			HandleInputAxisRaw();
+			if (_horizontalAxis != 0) //rotation
+			{
+				_cart.AddTorque(transform.up * (_horizontalAxis * angular * 50f * Time.deltaTime));
+			}
 
 			_driftValue = DriftValue();
 			IsDrifting = CheckIsDrifting();
@@ -96,59 +100,48 @@ public class CartMovement : MonoBehaviour
 			CheckCrashed();
 			AddDriftScore();
 
+
 			//interpolating values
-			_tippingThreshold += (fixedTippingThreshold - _tippingThreshold) * _tippingDecaySpeed;
-			_driftBoost += (maxBoostStrength - _driftBoost) * _boostDecaySpeed;
+			_tippingThreshold += (fixedTippingThreshold - _tippingThreshold) * _tippingDecaySpeed * Time.deltaTime;
+			_driftBoost += (maxBoostStrength - _driftBoost) * _boostDecaySpeed * Time.deltaTime;
 
 			HandleBoostParticles();
 
+			
 			//Debug.Log(_cart.velocity.magnitude + ", " +_cart.angularVelocity.magnitude);
-			return; // You should not perform any other action in FixedUpate if ragdoll is false
 		}
 
-		if (_propUp)
-		{
-			if (Vector3.Distance(transform.position, _propUpTargetPosition) <= 0.02f && Quaternion.Angle(transform.rotation, _lastRot) <= 4f)
-			{
-				ActivateNormal();
-				return;
-			}
-			MakeUpright();
-		}
-		else
-		{
-			if (Vector3.Dot(transform.up, Vector3.up) > 0.9f && _cart.velocity.magnitude < 0.1f && _cart.angularVelocity.magnitude < 0.1f)
-			{
-				ActivateNormal();
-			}
+		else if(Input.GetKeyDown("r")){
+			_propUpTargetPosition = transform.position + new Vector3(0, 1, 0);
+			_cart.isKinematic = true;
+			_propUp = true;
+			// _lastRot.y = 0;
+			_lastRot.x = 0;
+			_lastRot.z = 0;
 		}
 	}
+    private void FixedUpdate()
+    {
+	    if (!ragdoll) return;
+	    if (_propUp)
+	    {
+		    if (Vector3.Distance(transform.position, _propUpTargetPosition) <= 0.02f && Quaternion.Angle(transform.rotation, _lastRot) <= 4f)
+		    {
+			    ActivateNormal();
+			    return;
+		    }
+		    MakeUpright();
+	    }
+	    else
+	    {
+		    if (Vector3.Dot(transform.up, Vector3.up) > 0.9f && _cart.velocity.magnitude < 0.1f && _cart.angularVelocity.magnitude < 0.1f)
+		    {
+			    ActivateNormal();
+		    }
+	    }
+    }
 
-	private void HandleInputAxisRaw()
-	{
-		_verticalAxis = Input.GetAxisRaw("Vertical");
-		_horizontalAxis = Input.GetAxisRaw("Horizontal");
-
-		_driftValue = DriftValue();
-		switch (_verticalAxis)
-		{
-			//thrust
-			case > 0:
-				_cart.AddForce(transform.forward * (_verticalAxis * thrust * _driftBoost * 110f));
-				break;
-			//brake
-			case < 0:
-				_cart.AddForce(transform.forward * (_verticalAxis * thrust * _driftBoost * 40f));
-				break;
-		}
-
-		if (_horizontalAxis != 0) //rotation
-		{
-			_cart.AddTorque(transform.up * (_horizontalAxis * angular * 50f));
-		}
-	}
-
-	private float DriftValue()
+    private float DriftValue()
 	{
 		return Mathf.Abs(Vector3.Dot(transform.right, _cart.velocity) * ((1 + (weight / weightMax)) / 2));
 	}
@@ -173,7 +166,7 @@ public class CartMovement : MonoBehaviour
 
 			return true;
 		}
-
+		
 		foreach (WheelBehaviour wheel in wheels)
 		{
 			wheel.StopSmoke();
@@ -182,8 +175,7 @@ public class CartMovement : MonoBehaviour
 		return false;
 	}
 
-	private void CheckUpright()
-	{
+	private void CheckUpright(){
 		IsUpright = Vector3.Dot(transform.up, Vector3.up) > 0.9f;
 	}
 
@@ -193,16 +185,16 @@ public class CartMovement : MonoBehaviour
 		if (!IsUpright)
 		{
 
-			_floorDetected = Physics.Raycast(_raycastOrigin.position, _raycastOrigin.right, 0.45f, LayerMask.GetMask("Environment"));
-			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + _raycastOrigin.right * 0.45f, Color.red);
-			_floorDetected = _floorDetected || Physics.Raycast(_raycastOrigin.position, -_raycastOrigin.right, 0.45f, LayerMask.GetMask("Environment"));
-			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + -_raycastOrigin.right * 0.45f, Color.red);
-			_floorDetected = _floorDetected || Physics.Raycast(_raycastOrigin.position, _raycastOrigin.forward, 0.85f, LayerMask.GetMask("Environment"));
-			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + _raycastOrigin.forward * 0.85f, Color.red);
-			_floorDetected = _floorDetected || Physics.Raycast(_raycastOrigin.position, -_raycastOrigin.forward, 0.85f, LayerMask.GetMask("Environment"));
-			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + -_raycastOrigin.forward * 0.85f, Color.red);
+			_floorDetected = Physics.Raycast(_raycastOrigin.position, _raycastOrigin.right , 0.45f, LayerMask.GetMask("Environment"));
+			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + _raycastOrigin.right * 0.45f,  Color.red);
+			_floorDetected = _floorDetected || Physics.Raycast(_raycastOrigin.position, -_raycastOrigin.right , 0.45f, LayerMask.GetMask("Environment"));
+			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + -_raycastOrigin.right * 0.45f,  Color.red);
+			_floorDetected = _floorDetected || Physics.Raycast(_raycastOrigin.position, _raycastOrigin.forward , 0.85f, LayerMask.GetMask("Environment"));
+			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + _raycastOrigin.forward * 0.85f,  Color.red);
+			_floorDetected = _floorDetected || Physics.Raycast(_raycastOrigin.position, -_raycastOrigin.forward , 0.85f, LayerMask.GetMask("Environment"));
+			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + -_raycastOrigin.forward * 0.85f,  Color.red);
 			_floorDetected = _floorDetected || Physics.Raycast(_raycastOrigin.position, _raycastOrigin.up, 0.45f, LayerMask.GetMask("Environment"));
-			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + _raycastOrigin.up * 0.45f, Color.red);
+			Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.position + _raycastOrigin.up * 0.45f,  Color.red);
 			if (_floorDetected)
 			{
 				ActivateRagdoll();
@@ -217,7 +209,7 @@ public class CartMovement : MonoBehaviour
 		_cart.isKinematic = false;
 		_boxCollider.material = slipperyMaterial;
 		_propUp = false;
-		_capsuleCollider.material = slipperyMaterial;
+		_capsuleCollider.material =slipperyMaterial;
 	}
 
 	private void ActivateRagdoll()
@@ -239,7 +231,7 @@ public class CartMovement : MonoBehaviour
 	{
 		if (IsDrifting)
 		{
-			_driftScore += _driftValue * 10;
+			_driftScore += _driftValue * Time.deltaTime * 10;
 			if (_driftScore > minBoost)
 			{
 				BoostReady = true;
@@ -253,7 +245,7 @@ public class CartMovement : MonoBehaviour
 		{
 			if (BoostReady)
 			{
-				_driftBoost = 1 + Mathf.Min(_driftScore * maxBoostStrength / 200, maxBoostStrength);
+				_driftBoost = 1  + Mathf.Min(_driftScore * maxBoostStrength / 200, maxBoostStrength);
 				_tippingThreshold = fixedTippingThreshold + Mathf.Min(_driftScore * maxTippingThresholdBoost / 200, maxTippingThresholdBoost);
 				Debug.Log("boost: tipping threshold: min of(" + (_driftScore * maxTippingThresholdBoost / 200) + ", " + maxTippingThresholdBoost + "), driftBoost: " + _driftBoost);
 			}
@@ -266,34 +258,31 @@ public class CartMovement : MonoBehaviour
 	public void AddWeight(int add)
 	{
 		if (weight + add > weightMax) return;
-
+		
 		weight += add;
 		UpdateWeight();
 	}
 
 	private void UpdateWeight()
 	{
-		_cart.mass = physicalBaseWeight + weight * 0.2f;
-		fixedTippingThreshold = emptyTippingThreshold - ((emptyTippingThreshold - fullTippingThreshold) / weightMax * weight);
+		_cart.mass = physicalBaseWeight + weight*0.2f;
+		fixedTippingThreshold = emptyTippingThreshold - ((emptyTippingThreshold - fullTippingThreshold)/weightMax * weight);
 	}
 
-	private void HandleBoostParticles()
-	{
-		if (_driftBoost > 1.1f)
-		{
+	private void HandleBoostParticles(){
+		if (_driftBoost > 1.1f){
 			boostParticles.Play();
 		}
-		else
-		{
+		else{
 			boostParticles.Stop();
 		}
 	}
-
+	
 	private void MakeUpright() //raises the cart and changes its rotation to the last rotation before crashing
 	{
 		// Quaternion.RotateTowards approximates pi so we stop rotating once the angle becomes close to pi
 
 		transform.position = Vector3.SmoothDamp(transform.position, _propUpTargetPosition, ref _vel, 0.5f);
-		transform.rotation = Quaternion.RotateTowards(transform.rotation, _lastRot, 2f);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, _lastRot, 2f );
 	}
 }
