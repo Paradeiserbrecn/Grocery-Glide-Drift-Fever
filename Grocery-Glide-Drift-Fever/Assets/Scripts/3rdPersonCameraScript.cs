2ce09af3138d2faf2122346afd158ec86cdaf528
SirSkoootsch <147728645+SirSkoootsch@users.noreply.github.com>
2025-06-03 13:48:03 +0200

    Movement tweaking (#18)
    * if you crash you crash
    
    * Supadupa megageile scheiße
    
    DRIIFT 📢
    
    * made some boost adjustments
    
    * tire tracks (not fully functional)
    
    * tire tracks now work and wheel refactoring
    
    particle control was half in the wheels and half in the cart controller and is now only in the cart controller
    
    * upsiiii i fucked the spark logic :,,))) its gud now
    
    * idk how but movement was not using delta time yet.. so now it does
    
    * boost particles
    
    * collecting items partially works
    
    weight and physical weight updates, tipping theshold does not update yet
    
    * fixed some minor bugs with picking up items+ tipping theshold updates now too
    
    * set up some design stuff
    
    * logo stuff
    
    * Should remove deltatime redundancy again introduced in #840ca04
    
    FixedUpdate was already here so we can just use it
    
    * Revert "Should remove deltatime redundancy again introduced in #840ca04"
    
    This reverts commit 6eb5b9999b04f0183ef33c78536614e1f15bec09.
    
    * Did some optimization and refactoring
    
    * So hetzan is des mit dem aufnehmen guat
    
    weil is jetz guat und nemma schlecht neinein
    
    * finished logo draft 1
    
    * Revert "finished logo draft 1"
    
    This reverts commit 41af15eca88de90059b51f306dcc8ec1f8a02647.
    
    ---------
    
    Co-authored-by: Scar <scarcheekttgm@gmail.com>

--- Grocery-Glide-Drift-Fever/Assets/Scripts/3rdPersonCameraScript.cs
+++ Grocery-Glide-Drift-Fever/Assets/Scripts/3rdPersonCameraScript.cs
@@ -8,78 +8,74 @@ using UnityEngine.UIElements;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-	[SerializeField] private CartMovement cart;
-	private Transform _cartTransform;
-	[SerializeField] private float dist = 5.0f;
-	[SerializeField] private float ragdollDist = 10.0f;
-	[SerializeField] private float height = 0.5f;
-	[SerializeField] private float smooth = 0.1f;
-	[SerializeField] private float lookHeight = 1f;
-	private Vector3 _vel = Vector3.zero;
-	private Vector3 _ragdollVel = Vector3.zero;
-	private Vector3 _dirVel = Vector3.zero;
-	private Vector3 _ragdollDirVel = Vector3.zero;
-	private Vector3 _globalCameraSpacingVector;
-	private Vector3 _globalCameraPos;
-	private Vector3 _lookPoint;
-	private Vector3 _direction;
+    [SerializeField] private CartMovement cart;
+    private Transform _cartTransform;
+    [SerializeField] private float dist = 5.0f;
+    [SerializeField] private float ragdollDist = 6.0f;
+    [SerializeField] private float height = 0.5f;
+    [SerializeField] private float smooth = 0.1f;
+    [SerializeField] private float lookHeight = 1f;
+    private Vector3 _vel = Vector3.zero;
+    private Vector3 _ragdollVel = Vector3.zero;
+    private Vector3 _ragdollDirVel = Vector3.zero;
+    private Vector3 _globalCameraSpacingVector;
+    private Vector3 _globalCameraPos;
+    private Vector3 _lookPoint;
+    private Vector3 _direction;
 
-	private RaycastHit hitInfo = new RaycastHit();
-	private RaycastHit ragdolHitInfo = new RaycastHit();
+    private RaycastHit _hitInfo;
+    private RaycastHit _ragdollHitInfo;
 
-	private void Start()
-	{
-		_cartTransform = cart.GetComponent<Transform>();
-	}
+    private void Start()
+    {
+        _cartTransform = cart.GetComponent<Transform>();
+    }
 
-	private void Update()
-	{
-		if (!cart.ragdoll)
-		{
-			_globalCameraSpacingVector = -_cartTransform.forward * dist;
-			_globalCameraSpacingVector.y = height;
+    private void Update()
+    {
+        if (!cart.ragdoll)
+        {
+            _globalCameraSpacingVector = -_cartTransform.forward * dist;
+            _globalCameraSpacingVector.y = height;
 
-			_globalCameraPos = _cartTransform.position + _globalCameraSpacingVector;
-			_direction =  _globalCameraPos - _lookPoint;
-			
-			//This makes the camera avoid terrain, so you do not end up looking at a wall
-			_lookPoint = _cartTransform.position - _cartTransform.up * lookHeight;
-			// Debug.DrawLine(_lookPoint, _globalCameraPos);
-			bool deiMama = Physics.Raycast(_lookPoint, _direction , out hitInfo, _direction.magnitude, LayerMask.GetMask("Default"));
-			
-			if (deiMama)
-			{
-				//Debug.Log("Hallo freunde, ich darf doch freunde sagen?!");
-				_globalCameraPos = hitInfo.point+ hitInfo.normal * 0.2f;
-			}
-			
-			transform.position = Vector3.SmoothDamp(transform.position, _globalCameraPos, ref _vel, smooth);
-			transform.forward = (_cartTransform.position - (transform.position + _cartTransform.up * lookHeight)).normalized;
+            _globalCameraPos = _cartTransform.position + _globalCameraSpacingVector;
+            _direction = _globalCameraPos - _lookPoint;
 
-		}
-		else
-		{
-			_globalCameraPos =  _cartTransform.position + (transform.position - _cartTransform.position).normalized * ragdollDist;
-			_direction = _globalCameraPos - _cartTransform.position;
-			
-			
-			_lookPoint = _cartTransform.position - _cartTransform.up * lookHeight;
-			
-			Debug.Log("Dei mama is so groß wie des: " + (transform.position - _cartTransform.position));
-			Debug.DrawLine(_lookPoint, _globalCameraPos,  Color.red);
-			bool deiMama = Physics.Raycast(_lookPoint, _direction , out ragdolHitInfo, _direction.magnitude, LayerMask.GetMask("Default"));
-			if (deiMama)
-			{
-				Debug.Log("Hallo freunde, ich darf doch freunde sagen?!");
-				_globalCameraPos = ragdolHitInfo.point+ ragdolHitInfo.normal * 0.2f;
-			}
-			Debug.DrawLine(_lookPoint, _globalCameraPos, Color.blue);
+            //This makes the camera avoid terrain, so you do not end up looking at a wall
+            _lookPoint = _cartTransform.position - _cartTransform.up * lookHeight;
+            // Debug.DrawLine(_lookPoint, _globalCameraPos);
+            bool raycastHit = Physics.Raycast(_lookPoint, _direction, out _hitInfo, _direction.magnitude,
+                LayerMask.GetMask("Environment"));
 
-			transform.position = Vector3.SmoothDamp(transform.position, _globalCameraPos, ref _ragdollVel, smooth);
-			// transform.forward = (_cartTransform.position - (transform.position)).normalized;
+            if (raycastHit)
+            {
+                _globalCameraPos = _hitInfo.point + _hitInfo.normal * 0.2f;
+            }
 
-			transform.forward = Vector3.SmoothDamp(transform.forward, (_cartTransform.position - (transform.position)).normalized, ref _ragdollDirVel, smooth);
-		}
+            transform.position = Vector3.SmoothDamp(transform.position, _globalCameraPos, ref _vel, smooth);
+            transform.forward = (_cartTransform.position - (transform.position + _cartTransform.up * lookHeight))
+                .normalized;
+        }
+        else
+        {
+            _globalCameraPos = _cartTransform.position +
+                               (transform.position - _cartTransform.position).normalized * ragdollDist;
+            _direction = _globalCameraPos - _cartTransform.position;
+            _lookPoint = _cartTransform.position - _cartTransform.up * lookHeight;
 
-	}
+            Debug.DrawLine(_lookPoint, _globalCameraPos, Color.red);
+            bool deiMama = Physics.Raycast(_lookPoint, _direction, out _ragdollHitInfo, _direction.magnitude,
+                LayerMask.GetMask("Environment"));
+            if (deiMama)
+            {
+                _globalCameraPos = _ragdollHitInfo.point + _ragdollHitInfo.normal * 0.2f;
+            }
+
+            Debug.DrawLine(_lookPoint, _globalCameraPos, Color.blue);
+
+            transform.position = Vector3.SmoothDamp(transform.position, _globalCameraPos, ref _ragdollVel, smooth);
+            transform.forward = Vector3.SmoothDamp(transform.forward,
+                (_cartTransform.position - (transform.position)).normalized, ref _ragdollDirVel, smooth);
+        }
+    }
 }
--- Grocery-Glide-Drift-Fever/Assets/Scripts/CartInventoryScript.cs
+++ Grocery-Glide-Drift-Fever/Assets/Scripts/CartInventoryScript.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using Object = System.Object;
 
 public class CartInventory : MonoBehaviour
 {
-    private CartMovement cart;
+    [SerializeField] private CartMovement cart;
     private ArrayList inventory = new ArrayList();
+    [SerializeField] private CapsuleCollider itemRange;
+
+    private List<ShelfScript> collidingShelves = new List<ShelfScript>();
 
     private void AddItem(Item item)
     {
         inventory.Add(item);
         cart.AddWeight(item.Weight);
-        
+
+
         //TODO instance prefab in cart
         //TODO change sound in audio
     }
@@ -24,5 +31,49 @@ public class CartInventory : MonoBehaviour
 
         //TODO get rid of prefab in cart
         //TODO change sound in audio
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (collidingShelves.Count > 0)
+            {
+                ShelfScript nearest = collidingShelves
+                    .OrderBy(shelf => Vector3.Distance(shelf.transform.position, this.transform.position))
+                    .First();
+                if (nearest.hasItem)
+                {
+                    AddItem(nearest.Item);
+                    nearest.TakeItem();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Compares two shelves via their distance to the cart and follows the general CompareTo method functionality
+    /// </summary>
+    /// <param name="shelf">The shelf first shelf</param>
+    /// <param name="other">The other shelf to compare `shelf` to</param>
+    /// <returns> less than 0 if the distance to the cart of `shelf` is smaller than `other`.
+    /// 0 if the distance to the cart of `shelf` is exactly the same as `other`.
+    /// greater than 0 if the distance to the cart of `other` is greater than `shelf`</returns>
+    private int ShelfByDistanceComparator(ShelfScript shelf, ShelfScript other)
+    {
+        return Vector3.Distance(shelf.transform.position, this.transform.position)
+            .CompareTo(Vector3.Distance(other.transform.position, this.transform.position));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.GetComponentInParent<ShelfScript>());
+        collidingShelves.Add(other.GetComponentInParent<ShelfScript>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collidingShelves.Remove(other.GetComponentInParent<ShelfScript>());
     }
 }
