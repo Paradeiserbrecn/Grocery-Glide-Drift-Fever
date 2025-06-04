046eb0ec188fc54c6a3a028328c078efdc335504
SirSkoootsch <147728645+SirSkoootsch@users.noreply.github.com>
2025-06-04 15:45:23 +0200

    imported textmesh pro

---
+++ Grocery-Glide-Drift-Fever/Assets/Scripts/ShoppingList.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingList : MonoBehaviour
+{
+    private Dictionary<Item, ListItem> items = new Dictionary<Item, ListItem>();
+}
